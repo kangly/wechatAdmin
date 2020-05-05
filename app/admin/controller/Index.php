f4d8fd465263e890746dd5ae315420ae1f3af44a
kangly <371976974@qq.com>
2020-05-05 17:50:01 +0800

    基本配置

---
+++ app/admin/controller/Index.php
@@ -0,0 +1,15 @@
+<?php
+declare (strict_types = 1);
+
+namespace app\admin\controller;
+
+use app\BaseController;
+use think\facade\View;
+
+class Index extends BaseController
+{
+    public function index()
+    {
+        return View::fetch();
+    }
+}
--- config/app.php
+++ config/app.php
@@ -20,9 +20,12 @@ return [
     // 应用映射（自动多应用模式有效）
     'app_map'          => [],
     // 域名绑定（自动多应用模式有效）
-    'domain_bind'      => [],
+    'domain_bind'      => [
+        'my.wx' => 'index',
+        'admin.my.wx' => 'admin',
+    ],
     // 禁止URL访问的应用列表（自动多应用模式有效）
-    'deny_app_list'    => [],
+    'deny_app_list'    => ['index','admin'],
 
     // 异常页面的模板文件
     'exception_tmpl'   => app()->getThinkPath() . 'tpl/think_exception.tpl',
@@ -30,5 +33,5 @@ return [
     // 错误显示信息,非调试模式有效
     'error_message'    => '页面错误！请稍后再试～',
     // 显示错误信息
-    'show_error_msg'   => false,
+    'show_error_msg'   => true,
 ];
