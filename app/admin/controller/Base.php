1c5149061499cca79399b65367d95913de9c55ac
kangly <371976974@qq.com>
2020-05-06 15:03:32 +0800

    修复重定向bug、增加其他功能

--- app/admin/controller/Base.php
+++ app/admin/controller/Base.php
@@ -3,6 +3,7 @@
 namespace app\admin\controller;
 
 use app\BaseController;
+use think\exception\HttpResponseException;
 use think\facade\View;
 
 class Base extends BaseController
@@ -10,13 +11,25 @@ class Base extends BaseController
     public $adminInfo = [];//当前登录用户信息
 
     //初始化数据
-    public function initialize()
+    protected function initialize()
     {
+        parent::initialize();
+
         $this->adminInfo = is_login();
         if(!$this->adminInfo){
-            return redirect((string) url('/login'));
+            return $this->redirectTo((string) url('/login'));
         }
         View::assign('adminInfo',$this->adminInfo);
+
         return true;
+    }
+
+    /**
+     * 自定义重定向方法
+     * @param mixed ...$args
+     */
+    protected function redirectTo(...$args)
+    {
+        throw new HttpResponseException(redirect(...$args));
     }
 }
--- app/admin/controller/Index.php
+++ app/admin/controller/Index.php
@@ -15,29 +15,27 @@ class Index extends Base
      * 加载微信公众号图文消息
      * @param Request $request
      * @return string
-     * @throws \EasyWeChat\Kernel\Exceptions\InvalidConfigException
      * @throws \GuzzleHttp\Exception\GuzzleException
      */
     public function index(Request $request)
     {
-        $page = (int) $request->param('page');
+        $page = (int) $request->param('page',1);
         $type = 'news';
         $count = 10;
         $config = Config::get('wx');
 
         try{
             $app = Factory::officialAccount($config);
-            $list = $app->material->list($type, ($page-1)*$count, $count*$page);
+            $list = $app->material->list($type, ($page-1)*$count, $count);
+            //dump($list['item']);
         }catch (\Exception $e){
-            dump($e->getMessage());
-            exit();
+            return $e->getMessage();
         }
 
         //数组分页
         $pages_data = [];
         if($list){
-            $current_page = $page ? $page : 1;
-            $p = Bootstrap::make($list, $count, $current_page, $list['total_count']);
+            $p = Bootstrap::make($list, $count, $page, $list['total_count']);
             $p->appends($_GET);
             $pages_data = $p->render();
         }
@@ -46,5 +44,66 @@ class Index extends Base
         View::assign('pages',$pages_data);
 
         return View::fetch();
+    }
+
+    /**
+     * @param Request $request
+     * @return string
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function update(Request $request)
+    {
+        $mediaId = $request->param('id');
+        if($mediaId){
+            $config = Config::get('wx');
+            try{
+                $app = Factory::officialAccount($config);
+                $resource = $app->material->get($mediaId);
+                View::assign('resource',$resource);
+            }catch (\Exception $e){
+                return $e->getMessage();
+            }
+        }
+
+        return View::fetch();
+    }
+
+    /**
+     * @param Request $request
+     * @return string
+     */
+    public function save(Request $request)
+    {
+        $mediaId = $request->param('id');
+        $index = (int) $request->param('i');
+        if($mediaId){
+            $config = Config::get('wx');
+            try{
+                $app = Factory::officialAccount($config);
+                /*$result = $app->material->updateArticle($mediaId, [
+                    'title' => '测试内容！',
+                    'thumb_media_id' => '', // 封面图片 mediaId
+                    'author' => 'kangly',
+                    'show_cover' => 1, // 是否在文章内容显示封面图片
+                    'digest' => '这里是文章摘要',
+                    'content' => '这里是文章内容，你可以放很长的内容',
+                    'source_url' => 'https://www.baidu.com',
+                ],$index);*/
+                return 'success';
+            }catch (\Exception $e){
+                return $e->getMessage();
+            }
+        }else{
+            return 'error';
+        }
+    }
+
+    /**
+     * @param Request $request
+     */
+    public function delete(Request $request)
+    {
+        $mediaId = $request->param('id');
+        $index = (int) $request->param('i');
     }
 }
